b7b8ad0cfe8720a3d092d8d408b90662f1556130
Warlock <internalmike@gmail.com>
2019-09-06 22:26:29 +0300

    error on non-minimal encoded chars

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ const UTF8_LEAD_BYTE_PATTERN: [u8; UTF8_SEQUENCE_MAX_LENGTH as usize] = [0x00, 0
 const UTF8_TAIL_BYTE_MASK: u8 = 0xC0;
 const UTF8_TAIL_BYTE_PATTERN: u8 = 0x80;
 const UTF8_TAIL_BYTE_VALUE_BITS: u8 = 6;
+const UTF8_NONZERO_BITS_MASK: [u32; UTF8_SEQUENCE_MAX_LENGTH as usize] = [0, 0x0780, 0xF800, 0x01F00000];
 fn to_utf8(item: u32, expected_tail_bytes_count: u8, actual_tail_bytes_count: u8) -> ArrayVec<[u8; UTF8_SEQUENCE_MAX_LENGTH as usize]> {
     let mut res = ArrayVec::new();
     let lead_byte = UTF8_LEAD_BYTE_PATTERN[expected_tail_bytes_count as usize] |
@@ -76,6 +77,9 @@ impl<'a, T: BufRead> Iterator for Utf8Chars<'a, T> {
                         }
                     }
                 }
+                if tail_bytes_count != 0 && item & UTF8_NONZERO_BITS_MASK[tail_bytes_count as usize] == 0 {
+                    return Some(Err((Utf8InvalidSequence(to_utf8(item, tail_bytes_count, tail_bytes_count)), None)));
+                }
                 match char::from_u32(item) {
                     None => Some(Err((Utf8InvalidSequence(to_utf8(item, tail_bytes_count, tail_bytes_count)), None))),
                     Some(item) => Some(Ok(item))
@@ -105,8 +109,8 @@ mod tests {
 
     #[test]
     fn read_valid_unicode() {
-        assert_eq!(vec!['A', 'B', 'c', 'd', ' ', 'А', 'Б', 'в', 'г', 'д', ' ', 'U', 'V'],
-                    BufReader::new("ABcd АБвгд UV".as_bytes()).utf8_chars().map(|x| x.unwrap()).collect::<Vec<_>>());
+        assert_eq!(vec!['A', 'B', 'c', 'd', ' ', 'А', 'Б', 'в', 'г', 'д', ' ', 'U', '\0'],
+                    BufReader::new("ABcd АБвгд U\0".as_bytes()).utf8_chars().map(|x| x.unwrap()).collect::<Vec<_>>());
     }
 
     #[test]
@@ -129,5 +133,21 @@ mod tests {
         assert_eq!(1, res.len());
         let err = res[0].as_ref().err().unwrap();
         assert_eq!(&[0xF5, 0x8F, 0xBF, 0xBF][..], err.0.as_bytes());
+    }
+
+    #[test]
+    fn read_invalid_sequences() {
+        let mut bytes = BufReader::new(&[ 0x81, 0x82, 0xC1, 0x07, 0xC1, 0x87, 0xC2, 0xC2, 0x82, 0xF7, 0x88, 0x89, 0x07 ][..]);
+        let res = bytes.utf8_chars().collect::<Vec<_>>();
+        assert_eq!(9, res.len());
+        assert_eq!(&[0x81][..], res[0].as_ref().err().unwrap().0.as_bytes());
+        assert_eq!(&[0x82][..], res[1].as_ref().err().unwrap().0.as_bytes());
+        assert_eq!(&[0xC1][..], res[2].as_ref().err().unwrap().0.as_bytes());
+        assert_eq!('\x07', *res[3].as_ref().unwrap());
+        assert_eq!(&[0xC1, 0x87][..], res[4].as_ref().err().unwrap().0.as_bytes());
+        assert_eq!(&[0xC2][..], res[5].as_ref().err().unwrap().0.as_bytes());
+        assert_eq!('\u{82}', *res[6].as_ref().unwrap());
+        assert_eq!(&[0xF7, 0x88, 0x89][..], res[7].as_ref().err().unwrap().0.as_bytes());
+        assert_eq!('\x07', *res[8].as_ref().unwrap());
     }
 }
