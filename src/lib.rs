afc310321fc8bcf9c0de3501085d363372d66228
Warlock <internalmike@gmail.com>
2019-09-07 05:06:44 +0300

    fill_buf_and_ignore_interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+#![feature(nll)]
 #![allow(non_shorthand_field_patterns)]
 extern crate arrayvec;
 
-use std::{fmt};
+use std::{fmt, slice};
 use std::char::{self};
 use std::io::{self, BufRead};
 use arrayvec::{ArrayVec};
@@ -29,23 +30,6 @@ impl fmt::Display for InvalidChar {
     }
 }
 
-const SEQUENCE_MAX_LENGTH: u8 = 4;
-const LEAD_BYTE_MASK: [u8; SEQUENCE_MAX_LENGTH as usize] = [0x80, 0xE0, 0xF0, 0xF8];
-const LEAD_BYTE_PATTERN: [u8; SEQUENCE_MAX_LENGTH as usize] = [0x00, 0xC0, 0xE0, 0xF0];
-const TAIL_BYTE_MASK: u8 = 0xC0;
-const TAIL_BYTE_PATTERN: u8 = 0x80;
-const TAIL_BYTE_VALUE_BITS: u8 = 6;
-const SEQUENCE_MIN_VALUE: [u32; SEQUENCE_MAX_LENGTH as usize] = [0, 0x80, 0x800, 0x10000];
-fn to_utf8(item: u32, expected_tail_bytes_count: u8, actual_tail_bytes_count: u8) -> ArrayVec<[u8; SEQUENCE_MAX_LENGTH as usize]> {
-    let mut res = ArrayVec::new();
-    let lead_byte = LEAD_BYTE_PATTERN[expected_tail_bytes_count as usize] |
-                        ((item >> (TAIL_BYTE_VALUE_BITS * expected_tail_bytes_count)) as u8) & !LEAD_BYTE_MASK[expected_tail_bytes_count as usize];
-    res.push(lead_byte);
-    for tail_byte_index in 0..actual_tail_bytes_count {
-        res.push(TAIL_BYTE_PATTERN | ((item >> ((expected_tail_bytes_count - 1 - tail_byte_index) * TAIL_BYTE_VALUE_BITS)) as u8) & !TAIL_BYTE_MASK);
-    }
-    res
-}
 impl<'a, T: BufRead> Iterator for Chars<'a, T> {
     type Item = Result<char, (InvalidChar, Option<io::Error>)>;
 
@@ -58,6 +42,39 @@ impl<'a, T: BufRead> Iterator for Chars<'a, T> {
     }
 }
 
+const SEQUENCE_MAX_LENGTH: u8 = 4;
+const LEAD_BYTE_MASK: [u8; SEQUENCE_MAX_LENGTH as usize] = [0x80, 0xE0, 0xF0, 0xF8];
+const LEAD_BYTE_PATTERN: [u8; SEQUENCE_MAX_LENGTH as usize] = [0x00, 0xC0, 0xE0, 0xF0];
+const TAIL_BYTE_MASK: u8 = 0xC0;
+const TAIL_BYTE_PATTERN: u8 = 0x80;
+const TAIL_BYTE_VALUE_BITS: u8 = 6;
+const SEQUENCE_MIN_VALUE: [u32; SEQUENCE_MAX_LENGTH as usize] = [0, 0x80, 0x800, 0x10000];
+
+fn to_utf8(item: u32, expected_tail_bytes_count: u8, actual_tail_bytes_count: u8) -> ArrayVec<[u8; SEQUENCE_MAX_LENGTH as usize]> {
+    let mut res = ArrayVec::new();
+    let lead_byte = LEAD_BYTE_PATTERN[expected_tail_bytes_count as usize] |
+                        ((item >> (TAIL_BYTE_VALUE_BITS * expected_tail_bytes_count)) as u8) & !LEAD_BYTE_MASK[expected_tail_bytes_count as usize];
+    res.push(lead_byte);
+    for tail_byte_index in 0..actual_tail_bytes_count {
+        res.push(TAIL_BYTE_PATTERN | ((item >> ((expected_tail_bytes_count - 1 - tail_byte_index) * TAIL_BYTE_VALUE_BITS)) as u8) & !TAIL_BYTE_MASK);
+    }
+    res
+}
+
+fn fill_buf_and_ignore_interrupts(reader: &mut impl BufRead) -> io::Result<&[u8]> {
+    let (buf_ptr, buf_len) = loop {
+        match reader.fill_buf() {
+            Ok(buf) => break (buf.as_ptr(), buf.len()),
+            Err(e) => {
+                if e.kind() != io::ErrorKind::Interrupted {
+                    return Err(e)
+                }
+            }
+        }
+    };
+    Ok(unsafe { slice::from_raw_parts(buf_ptr, buf_len) })
+}
+
 /// Extends [`BufRead`] with methods for reading chars.
 pub trait BufReadCharsExt : BufRead {
     /// Returns an iterator over the chars of this reader.
@@ -65,8 +82,8 @@ pub trait BufReadCharsExt : BufRead {
     /// The iterator returned from this function will yield instances of `Result<char, (InvalidChar, Option<io::Error>)>`.
     ///
     /// See `read_char` function for a description of possible `Err`s.
-    fn chars<'a>(&'a mut self) -> Chars<'a, Self> { Chars(self) }
-    
+    fn chars(&mut self) -> Chars<Self> { Chars(self) }
+
     /// Reads a char from the underlying reader. Returns
     /// - `Ok(Some(char))` if a char is succesfully readed,
     /// - `Ok(None)` if the stream has reached EOF before lead byte was readed,
@@ -74,14 +91,15 @@ pub trait BufReadCharsExt : BufRead {
     /// - `Err((incomplete_char, Some(e))` with non-empty `incomplete_char` and `e.kind() == io::ErrorKind::UnexpectedEof` if EOF occuried after some bytes readed,
     /// - and `Err((readed_bytes, Some(io_error)))` if an I/O error with kind differs from `ErrorKind::Interrupted` occuried.
     ///
-    /// If this function encounters an error of the kind `ErrorKind::Interrupted` then the error is ignored and the operation will continue.
+    /// If this function encounters an error of the kind `io::ErrorKind::Interrupted` then the error is ignored and the operation will continue.
     ///
     /// The `InvalidChar` can contain an empty byte sequence if an I/O error occurs when read a lead byte.
     fn read_char(&mut self) -> Result<Option<char>, (InvalidChar, Option<io::Error>)>;
 }
+
 impl<T: BufRead> BufReadCharsExt for T {
     fn read_char(&mut self) -> Result<Option<char>, (InvalidChar, Option<io::Error>)> {
-        match self.fill_buf() {
+        match fill_buf_and_ignore_interrupts(self) {
             Err(e) => return Err((InvalidChar(ArrayVec::new()), Some(e))),
             Ok(buf) => {
                 if buf.is_empty() { return Ok(None); }
@@ -97,7 +115,7 @@ impl<T: BufRead> BufReadCharsExt for T {
                 };
                 let mut item = ((lead_byte & !LEAD_BYTE_MASK[tail_bytes_count as usize]) as u32) << (TAIL_BYTE_VALUE_BITS * tail_bytes_count);
                 for tail_byte_index in 0..tail_bytes_count {
-                    match self.fill_buf() {
+                    match fill_buf_and_ignore_interrupts(self) {
                         Err(e) => return Err((InvalidChar(to_utf8(item, tail_bytes_count, tail_byte_index)), Some(e))),
                         Ok(buf) => {
                             if buf.is_empty() {
